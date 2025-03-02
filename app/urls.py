2163960e532072db4d99da458d02b3c8adccfb76
RodrigoOnoGalvao <rog3108@gmail.com>
2025-03-01 23:34:37 -0300

    🔧  atualização nas urls

--- app/urls.py
+++ app/urls.py
@@ -10,11 +10,11 @@ urlpatterns = [
     path('pessoas/create/', PessoaCreateView.as_view(), name='pessoa_create'),
     path('pessoas/update/<int:pk>/', PessoaUpdateView.as_view(), name='pessoa_update'),
     path('pessoas/delete/<int:pk>/', PessoaDeleteView.as_view(), name='pessoa_delete'),
-    path('pessoas/detail/<int:pk>/', PessoaDetailView.as_view(), name='pessoa_detail'),
+    path('pessoas/<int:pk>/', PessoaDetailView.as_view(), name='pessoa_detail'),
 
     path('moedas/', MoedaListView.as_view(), name='moeda_list'),
     path('moedas/create/', MoedaCreateView.as_view(), name='moeda_create'),
     path('moedas/update/<int:pk>/', MoedaUpdateView.as_view(), name='moeda_update'),
     path('moedas/delete/<int:pk>/', MoedaDeleteView.as_view(), name='moeda_delete'),
-    path('moedas/detail/<int:pk>/', MoedaDetailView.as_view(), name='moeda_detail'),
+    path('moedas/<int:pk>/', MoedaDetailView.as_view(), name='moeda_detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -10,23 +10,14 @@ def index(request):
     return render(request, 'index.html')
 # Fetch exchange rates from Fixer.io
 def atualizar_moedas():
-    url = "https://data.fixer.io/api/latest?access_key=01b2f336a27b597b50c6e31a564e90a3"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
-        data = response.json()
-
-        if data.get("success"):
-            for codigo, taxa in data["rates"].items():
-                moeda, created = Moeda.objects.get_or_create(codigo=codigo, defaults={"nome": codigo, "taxa_conversao": taxa})
-                if not created:
-                    moeda.taxa_conversao = taxa
-                    moeda.save()
-        else:
-            print("Error in API response:", data.get("error"))
-    except requests.exceptions.RequestException as e:
-        print("Error fetching data from Fixer.io:", e)
-
+    api = 'https://data.fixer.io/api/latest?access_key=01b2f336a27b597b50c6e31a564e90a3'
+    response = requests.get(api)
+    data = response.json()
+    moedas = []
+    for sigla, valor in data['rates'].items():
+        moeda = Moeda.objects.all(nome='Euro', sigla=sigla, valor=valor)
+        moedas.append(moeda)
+    return moedas
 # Moeda Views
 class MoedaListView(ListView):
     model = Moeda
